db38b88e2341b38888732bc9b21fc22768ea642d
chongyehsai <73055016+chongyehsai@users.noreply.github.com>
2024-11-27 10:13:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import os
 os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 
 # Initialize the language model and prompt template
-llm = ChatOpenAI(model='gpt-3.5-turbo')
+llm = ChatOpenAI(model='gpt-4o')
 str_parser = StrOutputParser()
 template = (
     "Please answer the questions based on the following content and your own judgment:\n"
